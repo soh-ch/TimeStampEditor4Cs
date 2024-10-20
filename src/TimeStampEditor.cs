a23070a84821e5419f783369bb8c92f1cf10f940
soh-ch <soichiro.cook@gmail.com>
2024-10-20 17:31:41 +0900

    フォームとラベルを追加

--- src/TimeStampEditor.cs
+++ src/TimeStampEditor.cs
@@ -1,3 +1,5 @@
+// csc /target:winexe TimeStampEditor.cs
+
 using System;
 using System.IO;
 using System.Linq;
@@ -6,7 +8,7 @@ using System.Windows.Forms;
 using System.Refrection;
 
 
-public FileDateTimeUpdatorForm : Form
+public FileDateTimeUpdaterForm : Form
 {
     private DateTimePicker datePicker;
     private DateTimePicker timePicker;
@@ -17,5 +19,24 @@ public FileDateTimeUpdatorForm : Form
     private Label lblChkList;
     private Label lblDragDrop;
     private Label lblDragDropShape;
-}
+    
+    // ユーザフォーム定義
+    public FileDatteTimeUpdaterForm()
+    {
+        // [1] メインフォーム定義
+        this.Text = "TimeStampEditor"
+        this.Size = new Sysetm.Drawing.Size(230,380)
+        this.MinimizeBox = false;
+        this.MaximizeBox = false;
+        this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        this.TopMost = true;
 
+        // [2] ラベル 日付指定メッセージ
+        lblDateTimePicker = new Label();
+        lblDateTimePicker.Text = "①日時を指定してください。";
+        lblDateTimePicker.Size = new System.Drawing.Size(220,15);
+        lblDateTimePicker.Location = new System.Drawing.Point(10,10,);
+        tthis.Controls.Add(lblDateTimePicker);
+
+    }
+}
