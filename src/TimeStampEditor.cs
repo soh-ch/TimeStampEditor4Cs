26916783a598c686c6729f87a1d262d68636299f
soh-ch <soichiro.cook@gmail.com>
2024-10-20 17:42:57 +0900

    TODO記載

--- src/TimeStampEditor.cs
+++ src/TimeStampEditor.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 using System.Refrection;
 
 
-public FileDateTimeUpdaterForm : Form
+public class FileDateTimeUpdaterForm : Form
 {
     private DateTimePicker datePicker;
     private DateTimePicker timePicker;
@@ -31,12 +31,54 @@ public FileDateTimeUpdaterForm : Form
         this.FormBorderStyle = FormBorderStyle.FixedDialog;
         this.TopMost = true;
 
-        // [2] ラベル 日付指定メッセージ
+        // [2] ラベル ①日付指定メッセージ
         lblDateTimePicker = new Label();
         lblDateTimePicker.Text = "①日時を指定してください。";
         lblDateTimePicker.Size = new System.Drawing.Size(220,15);
         lblDateTimePicker.Location = new System.Drawing.Point(10,10,);
         tthis.Controls.Add(lblDateTimePicker);
 
+        // [3] 日付ピッカー
+        
+        // [4] 時刻ピッカー
+
+        // [5] ラベル ②対象を選択してください(複数可)
+
+
+        // [6] チェックボックス 作成日時
+
+        // [7] チェックボックス 更新日時
+
+        // [8] チェックボックス アクセス日時
+
+        // [9] ラベル ③ファイルをドラッグアンドドロップ(複数可)
+
+        // [10] ラベル ここにファイルをドラッグアンドドロップ
+
+
+        // [11] ドロップ許容
+        this.AllowDrop = true;
+        this.DragEnter += new DragEventHandler(DragDropLabel_DragEnter);
+        this.DragDrop += new DragEventHandler(DragDropLabel_DragDrop);
     }
+
+    // カーソルがフォームエリアに入ったときの挙動
+    private void DragDropLabel_DragEnter(object Sender, DragEventArgs e)
+    {
+        e.Effect = DragDropEffects.All;
+    }
+
+    // ドラッグアンドドロップしたときの処理
+    private void DragDropLabel_DragDrop(object Sender, DragEventArgs e)
+    {
+
+    }
+
+    // Main処理
+    [STAThread]
+    public static void Main(){
+        Application.EnableVisualStyles():
+        Application.Run(new FileDateTimeUpdaterForm());
+    }
+    
 }
