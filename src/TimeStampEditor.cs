1bea03724f77ef3548d8adfe9bc430e7d58a0149
soh-ch <soichiro.cook@gmail.com>
2024-10-20 17:30:57 +0900

    ラベル作成

---
+++ src/TimeStampEditor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using System.Refrection;
+
+
+public FileDateTimeUpdatorForm : Form
+{
+    private DateTimePicker datePicker;
+    private DateTimePicker timePicker;
+    private CheckBox chkCreation;
+    private CheckBox chkModificaion;
+    private CheckBox chkAccess;
+    private Label lblDateTimePicker;
+    private Label lblChkList;
+    private Label lblDragDrop;
+    private Label lblDragDropShape;
+}
+
